a76b39bf0f1826637200cf57c9d2e58e48751f9c
Gpayanfr <109517822+Gpayanfr@users.noreply.github.com>
2024-04-01 20:54:41 +0200

    Delete Fonctions directory

--- Fonctions/Send_mails.py
+++
@@ -1,9 +0,0 @@
-import smtplib
-
-smtpObj = smtplib.SMTP('smtp.gmail.com', 587)
-smtpObj.starttls()
-smtpObj.login('blabla@gmail.com', '')
-
-smtpObj.sendmail('blabla@gmail.com', 'password','Subject: Plan de travail \n Au boulot')
-
-smtpObj.quit()
